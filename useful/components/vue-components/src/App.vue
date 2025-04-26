1abe03a0ba342645e06935a60e537f7f6875ad56
Dima Chukhlancev <htmlweb78@gmail.com>
2025-04-26 20:04:59 +0300

    Сделал папку, где буду делать компоненты нужные для меня, пока еще не определился с архитектурой

---
+++ useful/components/vue-components/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import DragNDropArea from './components/DragNDropArea.vue';
+
+</script>
+<template>
+  <div class="main">
+    asdjlsdljasldjalsdjlj
+    <DragNDropArea></DragNDropArea>
+  </div>
+</template>
+
+<style>
+.main{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 500px;
+  height: 500px;
+  outline: 1px solid red;
+  background-color: var(--md-sys-color-surface);
+}
+</style>
---
+++ useful/components/vue-components/src/components/DragNDropArea.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="DragNDropArea">
+        <div class="DragNDropAreaCover">
+            <div class="DragNDropAreaContent">
+                <span>Перетащи файлы сюда, чтобы загрузить.</span> 
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.DragNDropArea {
+    z-index: 100;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    left: 0;
+    top: 0;
+}
+
+.DragNDropAreaCover {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    background: rgba(255, 255, 255, 0.21);
+    backdrop-filter: blur(5.2px);
+    -webkit-backdrop-filter: blur(5.2px);
+}
+
+.DragNDropAreaContent {
+    position: absolute;
+    height: 90%;
+    width: 90%;
+    outline: 2px dashed var(--md-sys-color-primary);
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border-radius: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.DragNDropAreaContent span{
+    color: var(--md-sys-color-primary);
+    font-size: 1.2rem;
+    text-align: center;
+}
+</style>
