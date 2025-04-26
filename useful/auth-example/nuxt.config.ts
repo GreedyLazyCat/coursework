3bf74796ca21682129e49475dcf9b8348680c627
Dmitriy Chukhlantsev <htmlweb78@gmail.com>
2025-04-27 01:42:22 +0300

    Добавил мини приложение, в котором разберусь как пользоваться Nuxt Auth

---
+++ useful/auth-example/nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  modules: ['@sidebase/nuxt-auth', '@prisma/nuxt'],
+  auth:{
+    isEnabled: false
+  },
+  devtools: { enabled: true }
+})
---
+++ useful/auth-example/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <h1>Hello, world!</h1>
+</template>
+
+<style>
+html,
+body {
+    padding: 0;
+    margin: 0;
+}
+
+</style>
