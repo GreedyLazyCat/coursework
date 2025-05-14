5eb5c5255efb0720e03d2453a853e03527772b17
Dima Chukhlancev <htmlweb78@gmail.com>
2025-05-14 09:41:45 +0300

    Добавил модуль авторизации

--- cura/nuxt.config.ts
+++ cura/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/icon'],
+  modules: ['@nuxt/icon', 'nuxt-auth-utils'],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true }
 })
