29f7aeaa34bcea68b637383c9432aec03f334208
Dmitriy Chukhlantsev <htmlweb78@gmail.com>
2025-05-15 17:24:25 +0300

    Дебажу сервер

--- cura/server/api/login.post.ts
+++ cura/server/api/login.post.ts
@@ -1,3 +1,4 @@
+import { useLogger } from 'nuxt/kit'
 import { z } from 'zod'
 
 const bodySchema = z.object({
@@ -7,8 +8,12 @@ const bodySchema = z.object({
 
 export default defineEventHandler(async (event) => {
     const { username, password } = await readValidatedBody(event, bodySchema.parse)
+    console.log(`Logging in user ${username} password ${password}...`)
+    console.log(`DB_USER: ${process.env.DB_USER} DB_PASSWORD: ${process.env.DB_PASSWORD} DB_NAME: ${process.env.DB_NAME} DB_HOST: ${process.env.DB_HOST} DB_PORT: ${process.env.DB_PORT}`)
     const db = useDrizzle()
+    console.log('Connected to database')
     const fetchedUser = await db.select().from(tables.user).where(eq(tables.user.username, username))
+    console.log(`Fetched user: ${JSON.stringify(fetchedUser)}`)
     const errorObj = {
         username: 'Неправильное имя пользователя или пароль.'
     }
@@ -16,13 +21,16 @@ export default defineEventHandler(async (event) => {
 
         throw createError({ statusCode: 400, statusMessage: 'Bad Credentials', message: JSON.stringify(errorObj) })
     }
+    console.log('Checked that user exists')
     const user = fetchedUser[0]
     if (await verifyPassword(user.password, password)) {
+        console.log('Verified password')
         await setUserSession(event, {
             user: {
                 id: user.id
             }
         })
+        console.log('set user session')
         return { message: 'Logged in' }
     }
     throw createError({ statusCode: 400, statusMessage: 'Bad Credentials', message: JSON.stringify(errorObj) })
--- cura/server/utils/drizzle.ts
+++ cura/server/utils/drizzle.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/node-postgres'
+import { useLogger } from 'nuxt/kit'
 export { sql, eq, and, or } from 'drizzle-orm'
 
 import * as schema from '~/server/db/schema'
@@ -7,6 +8,8 @@ import * as schema from '~/server/db/schema'
 export const tables = schema
 
 export function useDrizzle() {
-  return drizzle(`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}` , { schema })
+  const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+  console.log(`Connecting to database at ${connectionString}`) 
+  return drizzle(connectionString, { schema })
 }
 
